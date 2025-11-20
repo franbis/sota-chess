70aa504c49f79fa16827114d559c64e6fbf0aa92
Francesco Biscazzo <11650573+franbis@users.noreply.github.com>
2025-11-20 19:37:33 +0100

    Iterate through each square to toggle the last move style class rather than using 'squareStyles' from 'react-chessboard' options which is meant for hardcoded styling

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
-import type { Color } from 'chess.js';
+import type { Color, Square } from 'chess.js';
 import { Chessboard, type PieceDropHandlerArgs, type PieceHandlerArgs } from 'react-chessboard';
 
 import type { GameState, GameStateMsgContent } from '../../shared/types/chess.types';
@@ -15,6 +15,8 @@ function App() {
 	const [gameMan, setGameMan] = useState<RemoteChessManager | null>(null);
 	const [color, setColor] = useState<Color>('w');
 	const [gameState, setGameState] = useState<GameState | null>(null);
+
+	const boardContRef = useRef<HTMLDivElement | null>(null);
 
 
 	const canDragPiece = ({ isSparePiece, piece, square }: PieceHandlerArgs)=>{
@@ -39,26 +41,35 @@ function App() {
 	}, []);
 
 
-	let squareStyles = {};
-	if (gameState?.lastMove) {
-		squareStyles = Object.values(gameState?.lastMove ?? {}).reduce((acc, k) => {
-			acc[k] = {
-				backgroundColor: '#8cff1847'
+	// Handle square elements on game state changes.
+	useEffect(() => {
+		if (boardContRef.current) {
+				const squares = [
+					...boardContRef.current.querySelectorAll('div[data-square]')
+				] as HTMLDivElement[];
+
+			if (gameState?.lastMove) {
+				// Toggle styling based on the last move squares.
+				const lastMoveSquares: Square[] = Object.values(gameState?.lastMove ?? {});
+				for (const s of squares) {
+					if (lastMoveSquares.includes(s.dataset.square as Square))
+						s.classList.add('last_move');
+					else
+						s.classList.remove('last_move');
+				}
 			}
-			return acc;
-		}, {});
-	}
+		}
+	}, [boardContRef.current, gameState]);
 
 
 	return (
-		<div id="chessboard_cont">
+		<div id='chessboard_cont' ref={boardContRef}>
 				<Chessboard
 					options={{
 						// Default to an empty board.
 						position: gameState?.fen ?? '',
 						canDragPiece,
-						onPieceDrop,
-						squareStyles
+						onPieceDrop
 					}}
 				/>
 		</div>
